68156ae9def73dcf7ffb2fc27de0bfdf10a31e5a
Mircea Amarascu <mircea@ag-projects.com>
2007-08-24 14:58:59 +0000

    Basic authentication supports both plain and hashed passwords.

--- xcap/tweaks.py
+++ xcap/tweaks.py
@@ -1,6 +1,24 @@
-from twisted.cred import credentials
+import md5
 
-## Redefine 
+from twisted.cred import credentials
+from twisted.web2.auth.digest import IUsernameDigestHash
+
+from zope.interface import implements, Interface
+
+class BasicCredentials(credentials.UsernamePassword):
+    """Custom Basic Credentials, which support both plain and hashed checks."""
+
+    implements(credentials.IUsernamePassword, IUsernameDigestHash)
+
+    def __init__(self, username, password, realm):
+        self.username = username
+        self.password = password
+        self.realm = realm
+
+    def checkHash(self, digestHash):
+        s = '%s:%s:%s' % (self.username, self.realm, self.password)
+        return digestHash == md5.new(s).hexdigest()
+
 
 def decode(self, response, request):
     try:
@@ -10,8 +28,7 @@ def decode(self, response, request):
 
     creds = creds.split(':', 1)
     if len(creds) == 2:
-        creds = credentials.UsernamePassword(*creds)
-        creds.realm = self.realm
+        creds = BasicCredentials(creds[0], creds[1], self.realm) # our change
         return creds
     else:
         raise error.LoginFailed('Invalid credentials')
