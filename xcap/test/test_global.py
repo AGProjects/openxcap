2e4cc07584c827a479c65b9c31423bc73e328885
Denis Bilenko <denis@ag-projects.com>
2008-10-03 03:30:11 +0000

    use globaltree instead of get_global

--- xcap/test/test_global.py
+++ xcap/test/test_global.py
@@ -1,5 +1,4 @@
 from common import *
-from lxml import etree
 
 has_global = ['xcap-caps']
 no_global = set(apps) - set(has_global)
@@ -8,11 +7,21 @@ class TestGlobal(XCAPTest):
 
     def test_no_global(self):
         for app in no_global:
-            self.get_global(app, status=404)
+            self.get(app, status=404, globaltree=True)
+
+            # at the moment, no one authorized to do that
+            # NOTE, even though 404 would be also a valid response here, 401 should take priority
+            # 404 or 401?
+#             self.put(app, xml, status=401, globaltree=True)
+#             self.delete(app, status=401, globaltree=True)
 
     def test_has_global(self):
         for app in has_global:
-            self.get_global(app, status=200)
+            self.get(app, status=200, globaltree=True)
+
+#             # at the moment, no one authorized to do that
+#             #self.put(app, xml, status=401, globaltree=True)
+#             self.delete(app, status=401, globaltree=True)
 
 if __name__ == '__main__':
     runSuiteFromModule()
