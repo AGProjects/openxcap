2acc71d97b86e32393e7176464dc06412fe24329
Denis Bilenko <denis@ag-projects.com>
2008-10-06 02:12:38 +0000

    added extended_version()

--- xcap/__init__.py
+++ xcap/__init__.py
@@ -5,3 +5,15 @@
 
 __version__ = "1.0.2"
 __cfgfile__ = "config.ini"
+
+def extended_version():
+    try:
+        if patchlevel==1:
+            return __version__ + ' (+ 1 patch)'
+        elif patchlevel>1:
+            return __version__ + ' (+ %s patches)' % patchlevel
+    except NameError:
+        pass
+    return __version__
+
+# patchlevel is appended by setup.py
