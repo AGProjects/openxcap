ab493cb44b6e88cd846e4f9314ce6bd3df2d8555
Denis Bilenko <denis@ag-projects.com>
2008-10-13 08:06:49 +0000

    minor rephrase (to suppress pyflakes warnings)

--- xcap/__init__.py
+++ xcap/__init__.py
@@ -7,13 +7,13 @@ __version__ = "1.0.4"
 __cfgfile__ = "config.ini"
 
 def extended_version():
-    try:
-        if patchlevel==1:
-            return __version__ + ' (+ 1 patch)'
-        elif patchlevel>1:
-            return __version__ + ' (+ %s patches)' % patchlevel
-    except NameError:
-        pass
+    patchlevel = globals.get('patchlevel')
+    if patchlevel is None:
+        return __version__
+    elif patchlevel==1:
+        return __version__ + ' (+ 1 patch)'
+    elif patchlevel>1:
+        return __version__ + ' (+ %s patches)' % patchlevel
     return __version__
 
 # patchlevel is appended by setup.py
