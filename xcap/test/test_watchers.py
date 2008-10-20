20739a88e31adec00f4cbc5931b3aad2f38e24ae
Denis Bilenko <denis@ag-projects.com>
2008-10-20 16:59:35 +0000

    added test_watchers.py

---
+++ xcap/test/test_watchers.py
@@ -0,0 +1,28 @@
+from common import XCAPTest, runSuiteFromModule
+
+
+watchers = """<?xml version='1.0' encoding='utf-8'?>
+<watchers xmlns="http://openxcap.org/ns/watchers"/>"""
+
+class Test(XCAPTest):
+
+    def test_get(self):
+        self.get('watchers')
+        self.get('watchers', '/watchers')
+        self.get('watchers', globaltree=True, status=404)
+        self.get('watchers', '/watchers', globaltree=True, status=404)
+
+#     def test_put_not_allowed(self):
+#         self.put('watchers', watchers, status=405)
+#         self.put('watchers', watchers, '/watchers', status=405)
+#         self.put('watchers', watchers, globaltree=True, status=405)
+#         self.put('watchers', watchers, '/watchers', globaltree=True, status=405)
+
+#     def test_delete_not_allowed(self):
+#         self.delete('watchers', status=405)
+#         self.delete('watchers', '/watchers', status=405)
+#         self.delete('watchers', globaltree=True, status=405)
+#         self.delete('watchers', '/watchers', globaltree=True, status=405)
+
+if __name__ == '__main__':
+    runSuiteFromModule()
