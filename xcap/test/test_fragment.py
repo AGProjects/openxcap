fe0167a4ea152e1a654e097f4e323ec3cb8ace6b
Denis Bilenko <denis@ag-projects.com>
2008-10-27 04:56:34 +0000

    added test for check_xml_fragment

---
+++ xcap/test/test_fragment.py
@@ -0,0 +1,46 @@
+import common as c
+from urlparse import urlparse
+
+document = """<?xml version="1.0" encoding="UTF-8"?>
+<resource-lists xmlns="urn:ietf:params:xml:ns:resource-lists">
+<list xmlns:gm="http://www.ekiga.org">
+<entry uri="sip:500@ekiga.net">
+<gm:group>Foo</gm:group>
+</entry>
+</list>
+</resource-lists>"""
+
+# well-formed fragment that would've been rejected by XML parser because of
+# unbound namespace prefix
+fragment = """<entry uri="sip:xxx@yyyyy.net">
+   <gm:group>Test</gm:group>
+</entry>"""
+
+node = '/resource-lists/list/entry[@uri="sip:xxx@yyyyy.net"]'
+
+class FragmentTest(c.XCAPTest):
+
+    def test_success(self):
+        self.put('resource-lists', document)
+        self.put('resource-lists', fragment, node)
+
+    def test_errors(self):
+        self.put('resource-lists', document)
+
+        r = self.put('resource-lists', "<tag></bag>", node, status=409)
+        self.assertInBody(r, 'mismatched tag')
+
+        r = self.put('resource-lists', "<ta g></ta g>", node, status=409)
+        self.assertInBody(r, 'not well-formed (invalid token)')
+
+        r = self.put('resource-lists', "<ta g></ta g>", node, status=409)
+        self.assertInBody(r, 'not well-formed (invalid token)')
+
+        r = self.put('resource-lists', "<tag1/><tag2/>", node, status=409)
+        self.assertInBody(r, 'junk after document element')
+
+        r = self.put('resource-lists', "<tag\\></tag\\>", node, status=409)
+        self.assertInBody(r, 'not well-formed (invalid token)')
+
+if __name__ == '__main__':
+    c.runSuiteFromModule()
