7ac25b513096885f3c4b04c493fdde6b45cb37d6
Denis Bilenko <denis@ag-projects.com>
2009-01-11 06:50:47 +0000

    made it clear that add-openxcap-user.py just prints SQL INSERT request but does not create a new record (it confused at least one user)

--- scripts/add-openxcap-user.py
+++ scripts/add-openxcap-user.py
@@ -1,6 +1,16 @@
+"""This is an example that shows how a new user could be added to
+`subscriber' table. It does NOT actually create a new record in
+the database.
+"""
 import sys
 import md5
-username, domain, password = sys.argv[1:]
+
+print __doc__
+
+try:
+    username, domain, password = sys.argv[1:]
+except ValueError:
+    sys.exit('USAGE: %s username domain password' % sys.argv[0])
 hash = md5.new(":".join([username, domain, password])).hexdigest()
 query = """INSERT INTO subscriber (username, domain, password, ha1) VALUES ("%(username)s", "%(domain)s", "%(password)s", "%(hash)s");""" % locals()
 print query
