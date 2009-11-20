2b76fa816229f83c935dadfe045220641e280382
Saul Ibarra <saul@ag-projects.com>
2009-11-20 10:44:52 +0000

    Use common-policy schema for validating dialog-rules documents

--- xcap/appusage/__init__.py
+++ xcap/appusage/__init__.py
@@ -355,7 +355,7 @@ class DialogRulesApplication(ApplicationUsage):
     id = "dialog-rules"
     default_ns = "urn:ietf:params:xml:ns:dialog-rules"
     mime_type = "application/auth-policy+xml"
-    schema_file = 'dialog-rules.xsd'
+    schema_file = 'common-policy.xsd'
 
 def get_xpath(elem):
     """Return XPATH expression to obtain elem in the document.
