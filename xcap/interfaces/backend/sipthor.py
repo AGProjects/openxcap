56037dc8735dad291040bdc600acb16b09dfe382
Saul Ibarra <saul@ag-projects.com>
2011-08-22 10:14:34 +0000

    Fixed unicode issues with SIPThor backend

--- xcap/interfaces/backend/sipthor.py
+++ xcap/interfaces/backend/sipthor.py
@@ -523,9 +523,10 @@ class Storage(object):
 
     def _got_document(self, (doc, etag), check_etag):
         check_etag(etag)
-        return StatusResponse(200, etag, doc)
+        return StatusResponse(200, etag, doc.encode('utf-8'))
 
     def put_document(self, uri, document, check_etag):
+        document = document.decode('utf-8')
         self._normalize_document_path(uri)
         etag = make_random_etag(uri)
         result = self._database.put(uri, document, check_etag, etag)
