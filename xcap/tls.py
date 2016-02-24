dc3ccc280b0adb55397b4c1f93807c7c67420192
Saul Ibarra <saul@ag-projects.com>
2016-02-24 12:03:44 +0000

    Style

--- xcap/tls.py
+++ xcap/tls.py
@@ -8,7 +8,9 @@ from gnutls.crypto import X509Certificate,  X509PrivateKey
 from application import log
 from application.process import process
 
+
 class _FileError(Exception): pass
+
 
 def file_content(file):
     path = process.config_file(file)
@@ -23,10 +25,11 @@ def file_content(file):
     finally:
         f.close()
 
+
 class Certificate(object):
     """Configuration data type. Used to create a gnutls.crypto.X509Certificate object
        from a file given in the configuration file."""
-    def __new__(typ, value):
+    def __new__(cls, value):
         if isinstance(value, str):
             try:
                 return X509Certificate(file_content(value))
@@ -34,12 +37,13 @@ class Certificate(object):
                 log.warn("Certificate file '%s' could not be loaded: %s" % (value, str(e)))
                 return None
         else:
-            raise TypeError, 'value should be a string'
+            raise TypeError('value should be a string')
+
 
 class PrivateKey(object):
     """Configuration data type. Used to create a gnutls.crypto.X509PrivateKey object
        from a file given in the configuration file."""
-    def __new__(typ, value):
+    def __new__(cls, value):
         if isinstance(value, str):
             try:
                 return X509PrivateKey(file_content(value))
@@ -47,4 +51,4 @@ class PrivateKey(object):
                 log.warn("Private key file '%s' could not be loaded: %s" % (value, str(e)))
                 return None
         else:
-            raise TypeError, 'value should be a string'
+            raise TypeError('value should be a string')
