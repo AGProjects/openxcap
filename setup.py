7d3902593a762df9103fe85069c92d029ef32a82
Dan Pascu <dan@ag-projects.com>
2019-09-12 10:31:36 +0000

    Refactored debian packaging
    - Increased debian compatibility level to 11
    - Use pybuild as the debian build system
    - Updated build and package dependencies
    - Switched from init script to systemd service
    - Install README to the debian package docs
    - Removed no longer used pycompat and pyversions debian files
    - Removed no longer used openxcap.dirs and openxcap.install debian files
    - Install the configuration sample and extra data files from setup.py

--- setup.py
+++ setup.py
@@ -1,42 +1,48 @@
 #!/usr/bin/env python
 
 import os
-import re
+import xcap
 
 from distutils.core import setup
 
-
-def get_version():
-    return re.search(r"""__version__\s+=\s+(?P<quote>['"])(?P<version>.+?)(?P=quote)""", open('xcap/__init__.py').read()).group('version')
-
-def find_packages(toplevel):
-    return [directory.replace(os.path.sep, '.') for directory, subdirs, files in os.walk(toplevel) if '__init__.py' in files]
-
-setup(name         = "openxcap",
-      version      = get_version(),
-      author       = "AG Projects",
-      author_email = "support@ag-projects.com",
-      url          = "http://openxcap.org/",
-      description  = "An open source XCAP server.",
-      long_description = """XCAP protocol allows a client to read, write, and modify application
+long_description = """XCAP protocol allows a client to read, write, and modify application
 configuration data stored in XML format on a server. XCAP maps XML document
 sub-trees and element attributes to HTTP URIs, so that these components can
 be directly accessed by HTTP. An XCAP server is used by the XCAP clients to
 store data like Presence policy in combination with a SIP Presence server
 that supports PUBLISH/SUBSCRIBE/NOTIFY methods to provide a complete
 [http://www.tech-invite.com/Ti-sip-WGs.html#wg-simple SIP SIMPLE] server
-solution.""",
-      license      = "GPL",
-      platforms    = ["Platform Independent"],
-      classifiers  = [
-        "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: Service Providers",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
-        "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python",
-      ],
-      packages = find_packages('xcap'),
-      scripts  = ['openxcap'],
-      package_data = {'xcap.appusage': ['xml-schemas/*']},
-      )
+solution."""
 
+
+def find_packages(toplevel):
+    return [directory.replace(os.path.sep, '.') for directory, subdirs, files in os.walk(toplevel) if '__init__.py' in files]
+
+
+setup(
+    name='openxcap',
+    version=xcap.__version__,
+
+    description='XCAP server',
+    long_description=long_description,
+    url='http://openxcap.org/',
+
+    author='AG Projects',
+    author_email='support@ag-projects.com',
+
+    license='GPL',
+    platforms=['Platform Independent'],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Service Providers',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+    ],
+
+    packages=find_packages('xcap'),
+    package_data={'xcap.appusage': ['xml-schemas/*']},
+    data_files=[('/etc/openxcap', ['config.ini.sample']), ('/etc/openxcap/tls', ['tls/README'])],
+    scripts=['openxcap']
+)
