f2798e26f69373f5edcfb7f1e9567631bc7351ec
Tijmen de Mes <tijmen@ag-projects.com>
2025-03-14 09:57:10 +0000

    Updated init

--- xcap/__init__.py
+++ xcap/__init__.py
@@ -1,4 +1,15 @@
+__project__ = "openxcap"
+__fullname__ = "OpenXCAP"
+__version__ = "3.0.0"
+__author__ = "AG Projects"
+__author_email__ = "support@ag-projects.com"
+__description__ = "An open source XCAP server"
+__license__ = "GPL-3.0-or-later"
+__url__ = "https://github.com/AGProjects/openxcap"
+__status__ = "Development"
 
-__version__ = '2.7.1'
-__cfgfile__ = 'config.ini'
-
+# Additional metadata (optional)
+__maintainer__ = "AG Projects"
+__maintainer_email__ = "support@ag-projects.com"
+__docs_url__ = "https://github.com/AGProjects/openxcap"  # Link to project documentation
+ 
