63ec6d9b04c8c3efc89ef72927e316dd37a87019
Tijmen de Mes <tijmen@ag-projects.com>
2025-04-17 08:55:07 +0000

    Raised version

--- xcap/__init__.py
+++ xcap/__init__.py
@@ -1,6 +1,6 @@
 __project__ = "openxcap"
 __fullname__ = "OpenXCAP"
-__version__ = "3.0.0"
+__version__ = "3.1.0"
 __author__ = "AG Projects"
 __author_email__ = "support@ag-projects.com"
 __description__ = "An open source XCAP server"
