ac313a6b54cc04b60c5cd867ad12910aa72d5894
Tijmen de Mes <tijmen@ag-projects.com>
2025-05-06 09:44:16 +0000

    Updated website url

--- xcap/__init__.py
+++ xcap/__init__.py
@@ -5,7 +5,7 @@ __author__ = "AG Projects"
 __author_email__ = "support@ag-projects.com"
 __description__ = "An open source XCAP server"
 __license__ = "GPL-3.0-or-later"
-__url__ = "http://openxcap.org"
+__url__ = "https://openxcap.org"
 __repository__ = "https://github.com/AGProjects/openxcap"
 __status__ = "Development"
 
